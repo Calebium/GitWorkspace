967d680c9c9027fcc2405f9af8111d284138863d
Caleb Chan <Calebium@gmail.com>
2017-05-24 14:02:37 -0700

    Initial commit

---
+++ git-config.sh
@@ -0,0 +1,14 @@
+# Launch specified editor when git requires text entry
+git config --global core.editor 'emacs'
+
+# Push the current branch to its upstream branch
+git config --global push.default upstream
+
+# Display merge conflicts in diff3 format
+git config --global merge.conflictstyle diff3
+
+# Set user email
+git config --global user.email 'Calebium@gmail.com'
+
+# Set user name
+git config --global user.name 'Caleb Chan'
